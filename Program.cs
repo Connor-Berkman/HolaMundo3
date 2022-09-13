dc456235785141924634e2b11a89edd3dc7e5a8b
Connor-Berkman <bcr346140@gmail.com>
2022-09-13 09:14:33 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo 3!!!");
+            Console.WriteLine("");
+            Console.WriteLine("That means hello world in spanish. :3");
+            Console.ReadKey(true);
+        }
+    }
+}
